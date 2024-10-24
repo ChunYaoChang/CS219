a97ae13d759b36686fbc2c43bf52f56e5a3a7f2a
ChunYaoChang <jim881028@gmail.com>
2024-10-24 16:46:37 -0700

    adaptive UI display

--- config.py
+++ config.py
@@ -1,27 +1,15 @@
-PAGE_TOP_STYLE = '''
-    <style>
-    
-           /* Remove blank space at top and bottom */ 
-           .block-container {
-               padding-top: 1rem;
-               padding-bottom: 0rem;
-            }
-           
-           /* Remove blank space at the center canvas */ 
-           .st-emotion-cache-z5fcl4 {
-               position: relative;
-               top: -62px;
-               }
-           
-           /* Make the toolbar transparent and the content below it clickable */ 
-           .st-emotion-cache-18ni7ap {
-               pointer-events: none;
-               background: rgb(255 255 255 / 0%)
-               }
-           .st-emotion-cache-zq5wmm {
-               pointer-events: auto;
-               background: rgb(255 255 255);
-               border-radius: 5px;
-               }
-    </style>
-'''
+PAGE_TOP_STYLE = """
+        <style>
+               .block-container {
+                    padding-top: 2rem;
+                    padding-bottom: 0rem;
+                    padding-left: 5rem;
+                    padding-right: 5rem;
+                }
+                .element-container:has(
+                    iframe[title="streamlit_js_eval.streamlit_js_eval"]
+                ) {
+                    display: none
+                }
+        </style>
+        """
--- gui.py
+++ gui.py
@@ -12,6 +12,7 @@ from streamlit_date_picker import date_range_picker, PickerType
 import time
 from pymongo import MongoClient
 import concurrent.futures
+from streamlit_js_eval import streamlit_js_eval
 
 from mobile_insight.analyzer.analyzer import *
 from mobile_insight.monitor import OfflineReplayer
@@ -19,11 +20,13 @@ from my_analyzer import my_analysis, download_bytes
 from config import PAGE_TOP_STYLE
 
 st.set_page_config(layout='wide')
-st.markdown(PAGE_TOP_STYLE, unsafe_allow_html=True)
-
+screen_inner_width, screen_inner_height = 0, 0
 st.title('MobileInsight-Cloud')
+st.markdown(PAGE_TOP_STYLE, unsafe_allow_html=True)
 cur_path = os.getcwd()
-
+screen_inner_width = streamlit_js_eval(js_expressions='parent.window.innerWidth', key = 'SCR')
+screen_inner_height = streamlit_js_eval(js_expressions='parent.window.innerHeight', key = 'SCR1')
+time.sleep(0.1)
 client = MongoClient('localhost', 27017)
 db = client['mobile_insight']
 
@@ -33,7 +36,6 @@ def transform_datetime(date_string):
     # Parse the string into a datetime object
     date_object = datetime.strptime(date_string, date_format)
     return date_object
-
 
 # @st.cache_data
 def load_data(filename):
@@ -77,6 +79,13 @@ def upload_log(uploaded_log):
             upsert=True  # Insert the document if it doesn't exist
         )
 
+# @st.cache_data
+def download_json(selected_json):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return json.dumps(selected_json, indent=4)
+
+def download_mi2log(args):
+    return download_bytes(args)
 
 display_tab, upload_tab, manage_files_tab = st.tabs(['Display', 'Upload', 'Manage Files'])
 
@@ -120,124 +129,10 @@ with upload_tab:
         time.sleep(2)
         st.session_state['file_uploader_key'] += 1
         st.rerun()
-        
-
-with display_tab:
-    filename_list = db['mi2log'].distinct('filename')
-    st.header(st.session_state['filename_selector'] if 'filename_selector' in st.session_state else filename_list[0])
-
-    if filename_list:
-        with st.popover('Filter'):
-
-            filename_selector = st.selectbox(
-                'Filename',
-                filename_list,
-                key='filename_selector'
-            )
-            keys_df = load_data(filename_selector)
-
-            type_id_selector = st.multiselect(
-                'type_id',
-                keys_df['type_id'].unique(),
-                []
-            )
-            if type_id_selector:
-                keys_df = keys_df[keys_df['type_id'].isin(type_id_selector)]
-
-            # st.markdown('timestamp')
-            # datetime_selector = date_range_picker(picker_type=PickerType.time,
-            #                                     start=keys_df['timestamp'].min() + timedelta(hours=8), end=keys_df['timestamp'].max() + timedelta(hours=8, seconds=1),
-            #                                     key='time_range_picker')
-                
-            # Format hour, minute, second options to be zero-padded
-            hours = [f'{i:02d}' for i in range(24)]
-            minutes = [f'{i:02d}' for i in range(60)]
-            seconds = [f'{i:02d}' for i in range(60)]
-
-            start_date_col, start_time_col = st.columns([0.5, 1])
-            start_date_selector = start_date_col.date_input('Start Date', keys_df['timestamp'].min())
-            start_hour_col, start_minute_col, start_second_col = start_time_col.columns(3)
-            start_hour_selector = start_hour_col.selectbox('Start Hour', options=hours, index=keys_df['timestamp'].min().hour)
-            start_minute_selector = start_minute_col.selectbox('Start Minute', options=minutes, index=keys_df['timestamp'].min().minute)
-            start_second_selector = start_second_col.selectbox('Start Second', options=seconds, index=keys_df['timestamp'].min().second)
-
-            end_date_col, end_time_col = st.columns([0.5, 1])
-            end_date_selector = end_date_col.date_input('End Date', keys_df['timestamp'].max())
-            end_hour_col, end_minute_col, end_second_col = end_time_col.columns(3)
-            end_hour_selector = end_hour_col.selectbox('End Hour', options=hours, index=keys_df['timestamp'].max().hour)
-            end_minute_selector = end_minute_col.selectbox('End Minute', options=minutes, index=keys_df['timestamp'].max().minute)
-            end_second_selector = end_second_col.selectbox('End Second', options=seconds, index=keys_df['timestamp'].max().second+1)
-
-            # Combine the date and time inputs for start and end time into a full datetime object
-            start_date = pd.to_datetime(f'{start_date_selector} {start_hour_selector}:{start_minute_selector}:{start_second_selector}')
-            end_date = pd.to_datetime(f'{end_date_selector} {end_hour_selector}:{end_minute_selector}:{end_second_selector}')
-
-            if start_date > end_date:
-                st.error('Start time cannot be later than end time.')
-
-            # date_format = '%Y-%m-%d %H:%M:%S'
-            # start_date = datetime.strptime(datetime_selector[0], date_format)
-            # end_date = datetime.strptime(datetime_selector[1], date_format)
-            keys_df = keys_df[(keys_df['timestamp'] >= start_date) & (keys_df['timestamp'] < end_date)]
-
-            keys_filtered_args = {
-                'filename': filename_selector,
-                'type_id': type_id_selector,
-                'start_date': start_date,
-                'end_date': end_date
-            }
-        left_column, right_column = st.columns(2)
-
-        # @st.cache_data
-        def download_json(selected_json):
-            # IMPORTANT: Cache the conversion to prevent computation on every rerun
-            return json.dumps(selected_json, indent=4)
-        
-        def download_mi2log(args):
-            return download_bytes(args)
-        
-        left_button, right_button = left_column.columns(2)
-        left_button.download_button(
-            label='Download Filtered mi2log File',
-            data=download_mi2log(keys_filtered_args),
-            file_name='filtered_log.mi2log',
-            mime='application/octet-stream',
-        )               
-
-        filtered_json_args = {
-            'type_id': {'$in': keys_filtered_args['type_id']},
-            'timestamp': {'$gte': keys_filtered_args['start_date'], '$lt': keys_filtered_args['end_date']},
-        }
-        filtered_json = list(db[filename_selector].find(filtered_json_args, {'_id': 0}))
-        for js in filtered_json:
-            js['timestamp'] = js['timestamp'].isoformat()
-
-        right_button.download_button(
-            label='Download Filtered JSON',
-            data=download_json(filtered_json),
-            file_name='filtered_log.json',
-            mime='application/json',
-        )
-
-        key_table = left_column.dataframe(keys_df[['type_id', 'timestamp', 'order']], on_select='rerun', selection_mode='single-row', height=600)
-            
-        if key_table['selection']['rows']:
-            selected_json = db[filename_selector].find_one(keys_df.iloc[key_table['selection']['rows'][0]].to_dict(), {'_id': 0})
-            selected_json['timestamp'] = selected_json['timestamp'].isoformat()
-
-            right_column.download_button(
-                label='Download Selected JSON',
-                data=download_json(selected_json),
-                file_name='selected_log.json',
-                mime='application/json',
-            )
-            right_column.json(selected_json)
-
-    else:
-        st.write('No records in MongoDB')
 
 with manage_files_tab:
-    files_df = pd.DataFrame(db['mi2log'].find({}, {'_id': 0, 'data': 0}))
+    files_df = pd.DataFrame(db['mi2log'].find({}, {'filename': 1, '_id': 0}).sort('upload_time', 1))
+    files_df.reset_index(drop=True, inplace=True)
     if not files_df.empty:
         files_table = st.dataframe(files_df, on_select='rerun', selection_mode='multi-row')
         left, mid, right = st.columns(10)[:3]
@@ -268,5 +163,105 @@ with manage_files_tab:
         if right.button('Delete All'):
             client.drop_database('mobile_insight')
             st.rerun()
+    else:
+        st.write('No records in MongoDB')
+
+with display_tab:
+    filename_list = [doc['filename'] for doc in db['mi2log'].find({}, {'filename': 1, '_id': 0}).sort('upload_time', 1)]
+
+    if filename_list:
+        with st.popover(f"`{st.session_state['filename_selector'] if 'filename_selector' in st.session_state else filename_list[0]}`"):
+
+            filename_selector = st.selectbox(
+                'Filename',
+                filename_list,
+                key='filename_selector'
+            )
+            keys_df = load_data(filename_selector)
+
+            type_id_selector = st.multiselect(
+                'type_id',
+                keys_df['type_id'].unique(),
+                []
+            )
+            if type_id_selector:
+                keys_df = keys_df[keys_df['type_id'].isin(type_id_selector)]
+
+            hours = [f'{i:02d}' for i in range(24)]
+            minutes = [f'{i:02d}' for i in range(60)]
+            seconds = [f'{i:02d}' for i in range(60)]
+
+            start_date_col, start_time_col = st.columns([0.5, 1])
+            start_date_selector = start_date_col.date_input('Start Date', keys_df['timestamp'].min())
+            start_hour_col, start_minute_col, start_second_col = start_time_col.columns(3)
+            start_hour_selector = start_hour_col.selectbox('Start Hour', options=hours, index=keys_df['timestamp'].min().hour)
+            start_minute_selector = start_minute_col.selectbox('Start Minute', options=minutes, index=keys_df['timestamp'].min().minute)
+            start_second_selector = start_second_col.selectbox('Start Second', options=seconds, index=keys_df['timestamp'].min().second)
+
+            end_date_col, end_time_col = st.columns([0.5, 1])
+            end_date_selector = end_date_col.date_input('End Date', keys_df['timestamp'].max())
+            end_hour_col, end_minute_col, end_second_col = end_time_col.columns(3)
+            end_hour_selector = end_hour_col.selectbox('End Hour', options=hours, index=keys_df['timestamp'].max().hour)
+            end_minute_selector = end_minute_col.selectbox('End Minute', options=minutes, index=keys_df['timestamp'].max().minute)
+            end_second_selector = end_second_col.selectbox('End Second', options=seconds, index=keys_df['timestamp'].max().second+1)
+
+            # Combine the date and time inputs for start and end time into a full datetime object
+            start_date = pd.to_datetime(f'{start_date_selector} {start_hour_selector}:{start_minute_selector}:{start_second_selector}')
+            end_date = pd.to_datetime(f'{end_date_selector} {end_hour_selector}:{end_minute_selector}:{end_second_selector}')
+
+            if start_date > end_date:
+                st.error('Start time cannot be later than end time.')
+
+            keys_df = keys_df[(keys_df['timestamp'] >= start_date) & (keys_df['timestamp'] < end_date)]
+
+            keys_filtered_args = {
+                'filename': filename_selector,
+                'type_id': type_id_selector,
+                'start_date': start_date,
+                'end_date': end_date
+            }
+
+            left_right_ratio = st.slider('Ratio', min_value=1, max_value=99, value=50)
+        left_column, right_column = st.columns([left_right_ratio, 100 - left_right_ratio])
+
+        
+        left_button, right_button = left_column.columns(2)
+        left_button.download_button(
+            label='Download Filtered mi2log File',
+            data=download_mi2log(keys_filtered_args),
+            file_name='filtered_log.mi2log',
+            mime='application/octet-stream',
+        )               
+
+        filtered_json_args = {
+            'type_id': {'$in': keys_filtered_args['type_id']},
+            'timestamp': {'$gte': keys_filtered_args['start_date'], '$lt': keys_filtered_args['end_date']},
+        }
+        filtered_json = list(db[filename_selector].find(filtered_json_args, {'_id': 0}))
+        for js in filtered_json:
+            js['timestamp'] = js['timestamp'].isoformat()
+
+        right_button.download_button(
+            label='Download Filtered JSON',
+            data=download_json(filtered_json),
+            file_name='filtered_log.json',
+            mime='application/json',
+        )
+
+        key_table = left_column.dataframe(keys_df[['type_id', 'timestamp', 'order']], on_select='rerun', selection_mode='single-row', width=screen_inner_width // 2, height=screen_inner_height - 330)
+            
+        if key_table['selection']['rows']:
+            selected_json = db[filename_selector].find_one(keys_df.iloc[key_table['selection']['rows'][0]].to_dict(), {'_id': 0})
+            selected_json['timestamp'] = selected_json['timestamp'].isoformat()
+
+            right_column.download_button(
+                label='Download Selected JSON',
+                data=download_json(selected_json),
+                file_name='selected_log.json',
+                mime='application/json',
+            )
+            with right_column.container(height=screen_inner_height - 330):
+                st.json(selected_json)
+
     else:
         st.write('No records in MongoDB')
